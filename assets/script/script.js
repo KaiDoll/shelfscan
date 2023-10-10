47e41b6bd48667f42d4fd1cf7c993bbc1520b435
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-10-10 13:11:33 -0500

    key & value stored in localstorage

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,6 +2,7 @@ var modal = document.getElementById("authentication-modal");
 var openBtn = document.getElementById("open-btn");
 var closeBtn = document.getElementById("close-btn");
 var searchBooksForm = document.querySelector('#search-books');
+var searchBtn = document.getElementById("search-btn");
 
 // var searchButton = document.getElementById("search-btn");//future use
 openBtn.onclick = function () {
@@ -16,3 +17,40 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+function passValues() {
+  var searchInput = document.getElementById("search-input").value;
+  var formatInput =document.getElementById('format-input').value;
+  localStorage.setItem('books', searchInput);
+  localStorage.setItem('genre', formatInput)
+  secondPage(); 
+}
+
+function secondPage() {
+  window.location.href = 'results.html';
+  console.log('bye');
+};
+console.log(searchBtn)
+searchBtn.addEventListener("click", function() {
+passValues();
+secondPage(); 
+});
+
+
+//add event listener to activate the change page function. 
+
+
+
+// function handleSearchBooksForm(event) {
+//   event.preventDefault();
+
+//   var searchInput =document.querySelector('#search-input').value;
+//   var formatInput =document.querySelector('#format-input').value;
+
+//   if (!searchInput) {
+//     console.error('We need a book name please!');
+//     return;
+//   } 
+
+//   // var searchingResult = 
+// }
