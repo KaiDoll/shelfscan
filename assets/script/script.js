81907d100074111ed3649eb361452a7d6fcca635
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-10-11 13:44:14 -0500

    JS update!

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,20 +3,14 @@ var openBtn = document.getElementById("open-btn");
 var closeBtn = document.getElementById("close-btn");
 var searchBooksForm = document.querySelector("#search-books");
 var searchBtn = document.getElementById("search-btn");
+var nyUrl = 'https://api.nytimes.com/svc/books/v3/lists.json?list-name=hardcover-fiction&api-key='
+var url = 'https://www.googleapis.com/books/v1/volumes?q='
 var nyApiKey = 'bR4y42iA1GzK1cKSkAc2M2cUAGpDbTrJ';
-var googleApiKey = 'AIzaSyCyaIcjiZ4WlpOaAKDLJVj04ytizEMCQYw';
+var googleApiKey = '&key=AIzaSyCyaIcjiZ4WlpOaAKDLJVj04ytizEMCQYw';
+var topSellingBooks = document.getElementById("top-shelf");
 
-// fetch('https://api.nytimes.com/svc/books/v3/lists.json?list-name=hardcover-fiction&api-key=' + nyApiKey, {
-//     method: 'get',
-//   })
-//   .then(response => { return response.json(); })
-//   .then(json => { console.log(json); });
 
-//   fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:1649374046&key=' + googleApiKey, {
-//     method: 'get'
-//   })
-//   .then(response => { return response.json(); })
-//   .then(json => { console.log(json); });
+
 
 openBtn.addEventListener('click', function () {
   modal.style.display = "block";
@@ -50,4 +44,38 @@ passValues();
 secondPage(); 
 
 });
+
+
+fetch( nyUrl + nyApiKey)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data){
+    console.log(data);
+    var isbn= data.results[0].isbns[0].isbn10;
+    topBookThumbnail(isbn)
+console.log(isbn)
+})
+
+function topBookThumbnail (isbn) {
+  console.log(isbn)
+  fetch(url + 'isbn:' + isbn + googleApiKey)
+  .then(function (response) {
+    console.log(response)
+    return response.json();
+  })
+  .then(function (data) {
+      console.log(data);
+   //need to make isbn var so it matches with the ny times. 
+  var thumbnail = data.items[0].volumeInfo.imageLinks.thumbnail;
+  console.log(thumbnail);
+for(var i = 0; i < data.items.length; i++) {
+  var frontPage = document.createElement('section');
+  var bookCover = document.createElement('img');
+  bookCover.classList.add('my-1', 'w-5/6', 'h-4/6');
+  bookCover.src = data.items[i].volumeInfo.imageLinks.thumbnail;
+
+  topSellingBooks.append(frontPage);
+  frontPage.append(bookCover);
+}})}
 
